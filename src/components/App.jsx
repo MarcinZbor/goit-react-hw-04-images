d3371abc6865d53ba176385b743605b978578d54
MarcinZbor <marcinzb@gazeta.pl>
2023-05-28 13:15:40 +0200

    useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,36 +20,30 @@ import styles from './App.module.css';
 //     clickedImg: '',
 //   };
 
-
 export const App = () => {
-const [inputValue, setInputValue] = useState("")
-const [picturesData, setPicturesData] = useState([])
-const [page, setPage] = useState(1)
-const [totalPages, setTotalPages] = useState(1)
-const [loading, setLoading] = useState(false)
-const [isModal, setIsModal] = useState(false)
-const [clickedImg, setClickedImg] = useState("")
+  const [inputValue, setInputValue] = useState('');
+  const [picturesData, setPicturesData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [Modal, setModal] = useState(false);
+  const [clickedImg, setClickedImg] = useState('');
 
-}
-
-
-const handleSubmit = inputQuery => {
-  setInputValue(inputQuery);
-  setPage(1);
-  setPicturesData([]);
-};
+  const handleSubmit = inputQuery => {
+    setInputValue(inputQuery);
+    setPage(1);
+    setPicturesData([]);
+  };
 
   const loaderClick = () => {
-    setPage(prevState =>  prevState + 1,
-    );
+    setPage(prevState => prevState + 1);
   };
 
   const handleImageClick = url => {
-    setModal(true),
-    setClickedImg(url)
+    setModal(true), setClickedImg(url);
   };
 
-  onModalClose = () => {
+  const onModalClose = () => {
     setModal(false);
   };
 
@@ -63,54 +57,49 @@ const handleSubmit = inputQuery => {
   //   ) {
   //     this.setState({ loading: true });
 
+  useEffect(() => {
+    getPicturesData(inputValue, page)
+      .then(response => {
+        if (response.data.hits.length === 0) {
+          toast.error(
+            'Sorry, there are no images matching your search query. Please try again.',
+            { theme: 'colored' }
+          );
+        } else {
+          this.setState(prevState => ({
+            picturesData: [...prevState.picturesData, ...response.data.hits],
+            totalPages: Math.ceil(
+              response.data.totalHits / response.data.hits.length
+            ),
+          }));
 
-
-      getPicturesData(inputValue, page)
-        .then(response => {
-          if (response.data.hits.length === 0) {
-            toast.error(
-              'Sorry, there are no images matching your search query. Please try again.',
-              { theme: 'colored' }
-            );
-          } else {
-            this.setState(prevState => ({
-              picturesData: [...prevState.picturesData, ...response.data.hits],
-              totalPages: Math.ceil(
-                response.data.totalHits / response.data.hits.length
-              ),
-            }));
-
-            if (this.state.page === 1) {
-              toast.info(
-                `Hooray! We found ${response.data.totalHits} images.`,
-                { theme: 'colored' }
-              );
-            }
+          if (this.state.page === 1) {
+            toast.info(`Hooray! We found ${response.data.totalHits} images.`, {
+              theme: 'colored',
+            });
           }
-        })
-        .catch(error => {
-          toast.error(error.message, { theme: 'colored' });
-        })
-        .finally(() => {
-          setLoading(false );
-        });
-      
-    
+        }
+      })
+      .catch(error => {
+        toast.error(error.message, { theme: 'colored' });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [inputValue, page]);
 
-
-    return (
-      <div className={styles.container}>
-        <Searchbar onSubmit={handleSubmit} />
-        {loading && <Loader />}
-        <ImageGallery data={picturesData} onClickImg={handleImageClick} />
-        {picturesData.length !== 0 && page !== totalPages && (
-          <Button onClick={loaderClick} />
-        )}
-        <ToastContainer autoClose={3000} />
-        {this.state.isModal && (
-          <Modal onModalClose={onModalClose}>
-            {clickedImg}
-          </Modal>
-        )}
-      </div>
-    );
+  return (
+    <div className={styles.container}>
+      <Searchbar onSubmit={handleSubmit} />
+      {loading && <Loader />}
+      <ImageGallery data={picturesData} onClickImg={handleImageClick} />
+      {picturesData.length !== 0 && page !== totalPages && (
+        <Button onClick={loaderClick} />
+      )}
+      <ToastContainer autoClose={3000} />
+      {this.state.isModal && (
+        <Modal onModalClose={onModalClose}>{clickedImg}</Modal>
+      )}
+    </div>
+  );
+};
